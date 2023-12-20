19230d16152584024c3664d5b8d559d8e7941a9c
Trusha Shah <trushashah1402@gmail.com>
2023-12-20 17:24:11 +0530

    update

--- src/Component/Register/Register.js
+++ src/Component/Register/Register.js
@@ -24,7 +24,7 @@ onNameChange = (event) => {
   }
 
   onSubmitRegister = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://smartbrainserver-1xr4.onrender.com/register', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
